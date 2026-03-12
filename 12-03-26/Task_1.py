75130f61c06f936a57ad2eecde882deea2634cb2
saanviagarwal864 <146159311+saanviagarwal864@users.noreply.github.com>
2026-03-12 13:21:06 +0530

    Create Task_1.py

---
+++ 12-03-26/Task_1.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from time import sleep
+driver = webdriver.Chrome()
+sleep(3)
+driver.get('https://www.supertails.com')
+sleep(3)
+driver.minimize_window()
+sleep(3)
+driver.maximize_window()
+sleep(3)
+driver.back()
+sleep(3)
+driver.forward()
+sleep(3)
+driver.refresh()
+sleep(3)
+driver.close()
+
+opts=webdriver.ChromeOptions()
+opts.add_experimental_option('detach',True)
+driver1=webdriver.Chrome(options=opts)
+driver1.get('https://www.supertails.com')
+driver1.maximize_window()
